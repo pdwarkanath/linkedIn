f5d52c8febe3fe5004c9388f777dbf6e0a674721
pdwarkanath <dwarkanath.prabhu@gmail.com>
2018-01-05 02:44:55 -0600

    absolute file cleaned up

--- linkedInAbs.py
+++ linkedInAbs.py
@@ -4,19 +4,10 @@ import time
 def easyApplyModal():
 	
 	#chooseRecent2Location = pyautogui.locateCenterOnScreen('ChooseRecent2.png')
-	
+	#time.sleep(0.2)
 	pyautogui.click(617,467)
-	
-
-	#uploadNew2Location = pyautogui.locateCenterOnScreen('UploadNew2.png')
-	pyautogui.click(515, 549)
-	
-	#selectResume()
-	time.sleep(0.5)
-	pyautogui.click(282, 305)
-	pyautogui.hotkey('alt','o')
-	time.sleep(0.1)
-
+	#time.sleep(0.5)
+	pyautogui.click(717,467)
 	#submitAppLocation = pyautogui.locateCenterOnScreen('SubmitApplication.png')
 	
 	pyautogui.click(841, 660)
@@ -25,8 +16,21 @@ def easyApplyModal():
 	#X2Location = pyautogui.locateCenterOnScreen('X2.png')
 	
 	pyautogui.click(915,111)
+	time.sleep(0.2)
+	pyautogui.press('browserback')
 
-
+	
+	"""
+	uploadNew2Location = pyautogui.locateCenterOnScreen('UploadNew2.png')
+	pyautogui.click(uploadNew2Location)
+	
+	#selectResume()
+	time.sleep(1)
+	pyautogui.click(282, 305)
+	pyautogui.hotkey('alt','o')
+	time.sleep(0.1)
+	"""
+	
 def easyApplyNewPage():
 	#time.sleep(0.2)
 
@@ -50,21 +54,32 @@ def easyApplyNewPage():
 	pyautogui.click(982, 564)
 	time.sleep(0.5)
 	pyautogui.hotkey('ctrl','w')
+	time.sleep(0.2)
+	pyautogui.press('browserback')
 
+def easyApply():
+	#easyApplyLocation = pyautogui.locateCenterOnScreen('EasyApply.png')
+	pyautogui.click(432,470)
+	pyautogui.click(432,450)
+		
+	time.sleep(0.1)
+	submitAppLocation = pyautogui.locateCenterOnScreen('SubmitApplication.png')
+	
+	if submitAppLocation == None:
+		easyApplyNewPage()
+	else:
+		easyApplyModal()
 
+easyApply()
 
+"""
+for i in range(3):
+	x = 220
+	y = 300+i*140
+	pyautogui.click(x,y)
+	pyautogui.click(x,y+20)
+	pyautogui.click(x,y+20)
+	time.sleep(0.2)
+	easyApply()
 
-easyApplyLocation = pyautogui.locateCenterOnScreen('EasyApply.png')
-#if easyApplyLocation == None:
-	#pyautogui.hotkey('ctrl','w')
-#else:
-
-pyautogui.click(432,440)
-pyautogui.click(432,450)
-pyautogui.click(432,460)
-time.sleep(0.1)
-submitAppLocation = pyautogui.locateCenterOnScreen('SubmitApplication.png')
-if submitAppLocation == None:
-	easyApplyNewPage()
-else:
-	easyApplyModal()
+"""
---
+++ test.py
@@ -0,0 +1,3 @@
+import pyautogui
+pyautogui.click(400,400)
+pyautogui.press('browserback')
